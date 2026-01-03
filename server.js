410b8f9c6116b9c18dbc1706429123e9929fbcbf
Jeevan Rajkumar <jeevanr5125@gmail.com>
2026-01-03 21:44:06 +0530

    install dotenv

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 require("dotenv").config();
-const mongoose = require("mongoose");
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✔️"))
